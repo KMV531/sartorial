ec3d42f69ff8534e55c2ff4b70c40c7cfef8fef8
KMV531 <koladjamomovinnybrayan3@gmail.com>
2025-07-02 17:46:03 +0200

    Webhook permission error resolved

--- src/sanity/schemaTypes/orderType.ts
+++ src/sanity/schemaTypes/orderType.ts
@@ -1,3 +1,171 @@
+// /schemas/order.ts
+
+import { defineField, defineType } from "sanity";
+
+export const orderType = defineType({
+  name: "order",
+  title: "Order",
+  type: "document",
+  fields: [
+    defineField({
+      name: "transactionId",
+      title: "Transaction ID",
+      type: "string",
+    }),
+    defineField({
+      name: "transactionRef",
+      title: "Merchant Transaction Reference",
+      type: "string",
+    }),
+    defineField({
+      name: "partnerTransactionId",
+      title: "Partner Transaction ID",
+      type: "string",
+    }),
+    defineField({
+      name: "paymentStatus",
+      title: "Payment Status",
+      type: "string",
+    }),
+    defineField({
+      name: "paymentMethod",
+      title: "Payment Method",
+      type: "string",
+    }),
+    defineField({
+      name: "amount",
+      title: "Amount",
+      type: "number",
+    }),
+    defineField({
+      name: "currency",
+      title: "Currency",
+      type: "string",
+    }),
+    {
+      name: "customer",
+      title: "Customer Info",
+      type: "object",
+      fields: [
+        defineField({
+          name: "name",
+          title: "Name",
+          type: "string",
+        }),
+        defineField({
+          name: "email",
+          title: "Email",
+          type: "string",
+        }),
+        defineField({
+          name: "address",
+          title: "Address",
+          type: "string",
+        }),
+        defineField({
+          name: "phone",
+          title: "Phone",
+          type: "string",
+        }),
+        // Add other custom fields as needed
+      ],
+    },
+    defineField({
+      name: "payerName",
+      title: "Payer Name",
+      type: "string",
+    }),
+    defineField({
+      name: "payerAccountId",
+      title: "Payer Account ID",
+      type: "string",
+    }),
+    defineField({
+      name: "payerUserId",
+      title: "Payer User ID",
+      type: "string",
+    }),
+    defineField({
+      name: "payerNote",
+      title: "Payer Note",
+      type: "string",
+    }),
+    defineField({
+      name: "transactionTime",
+      title: "Transaction Time",
+      type: "datetime",
+    }),
+    defineField({
+      name: "createdAt",
+      title: "Created At",
+      type: "datetime",
+    }),
+    defineField({
+      name: "merchantAccountId",
+      title: "Merchant Account ID",
+      type: "string",
+    }),
+    defineField({
+      name: "merchantFee",
+      title: "Merchant Fee",
+      type: "number",
+    }),
+    defineField({
+      name: "netAmountReceived",
+      title: "Net Amount Received",
+      type: "number",
+    }),
+    defineField({
+      name: "receivingEntity",
+      title: "Receiving Entity Name",
+      type: "string",
+    }),
+    defineField({
+      name: "items",
+      title: "Ordered Items",
+      type: "array",
+      of: [
+        {
+          type: "object",
+          fields: [
+            defineField({
+              name: "product",
+              title: "Product",
+              type: "reference",
+              to: [{ type: "product" }],
+            }),
+            defineField({
+              name: "quantity",
+              title: "Quantity",
+              type: "number",
+            }),
+            defineField({
+              name: "price",
+              title: "Price",
+              type: "number",
+            }),
+            defineField({
+              name: "size",
+              title: "Size",
+              type: "string",
+            }),
+            defineField({
+              name: "color",
+              title: "Color",
+              type: "object",
+              fields: [
+                { name: "name", title: "Name", type: "string" },
+                { name: "value", title: "Value", type: "string" },
+              ],
+            }),
+          ],
+        },
+      ],
+    }),
+  ],
+});
+
+/* 
 // sanity/schema/order.ts
 import { defineField, defineType } from "sanity";
 
@@ -94,3 +262,5 @@ export const orderType = defineType({
     }),
   ],
 });
+
+*/
